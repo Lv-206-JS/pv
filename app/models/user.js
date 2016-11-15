fd18a11e53056642af6e856625757f169d3ca109
Sikora Vitalii <vitalii.sikora@gmail.com>
2016-11-15 22:25:58 +0200

    add user model

---
+++ app/models/user.js
@@ -0,0 +1,12 @@
+var mongoose    = require('mongoose');
+mongoose.connect('mongodb://localhost/RestApi');
+
+var Schema = mongoose.Schema;
+
+module.exports = mongoose.model('User', new Schema({
+        firstname: String,
+        lastname:  String,
+        email:     String,
+        password:  String
+}));
+
