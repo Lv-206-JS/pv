0bf21827b6f926d85bec8e5ec340c436659271c1
Anti-k <romanslyzebniy@gmail.com>
2016-11-18 17:30:11 +0200

    routes for authentication

--- app.js
+++ app.js
@@ -20,8 +20,30 @@ app.use(function (request, response, next) {
     next();
 });
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret : "MONTANA",
+    saveUninitialized: true,
+    resave : false
+}));
+
+
 app.use('/rest/hello', helloAPI);
 app.use('/rest/projects', projectAPI);
+
+// loading routes for authentication
+
+var index = require('./rest/index');
+var user = require('./rest/user');
+var login = require('./rest/authorization/login');
+var signup = require('./rest/authorization/signup');
 
 app.use(/\/project.*/, express.static('./index.html'));
 app.use(express.static(__dirname));
