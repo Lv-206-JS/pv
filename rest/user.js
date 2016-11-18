a394b676802b54002cb09664f339f08d561018f3
Anti-k <romanslyzebniy@gmail.com>
2016-11-18 18:09:59 +0200

    authentication set

---
+++ rest/user.js
@@ -0,0 +1,17 @@
+
+var express = require('express');
+var router = express.Router();
+var DB = require('../dummyDB');
+
+router.get('/user/:id', function(req, res, next) {
+  if(!req.session.user) return res.redirect('/login');
+  if(req.session.user.id == req.params.id ) return res.render('profile', req.session.user);
+  var user = DB.getUser(req.params.id)
+  if(!user)
+    return next();
+  res.render('user', user);
+});
+
+
+
+module.exports = router;
