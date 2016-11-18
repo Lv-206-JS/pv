5e0ebb390ee9cc6c0bc896e8c0c231495ebf04be
Katya Sharko <katyasharko@gmail.com>
2016-11-18 16:43:34 +0200

    add routes to project

--- rest/project.js
+++ rest/project.js
@@ -61,14 +61,40 @@ router.post('/', jsonParser, function (request, response) {
 });
 
 //update project
-router.put('/:id', function (request, response) {
+router.put('/:id', jsonParser, function (request, response) {
     var stub  = require('./project-stub.json');
+    var element;
+
+    for (var i = 0, len = stub.length; i < len; i++) {
+        if(stub[i].id == request.params.id) {
+            element = i;
+        }
+    }
+    stub[element].id = request.body.id;
+    stub[element].name = request.body.name;
+    // stub[element].calender.hoursPerWorkingDay = request.body.hoursPerWorkingDay;
+    // stub[element].tasks[0].taskIdA = equest.body.taskIdA;
+    // stub[element].tasks[0].dependencies[0].taskIdB = request.body.taskIdB;
+    // stub[element].tasks[0].dependencies[0].type = request.body.type;
+    // stub[element].tasks[1].taskIdB = equest.body.taskIdB;
+    // stub[element].tasks[1].dependencies = request.body.dependencies;
+     stub[element].milestones = request.body.milestones;
+     stub[element].attachments = request.body.attachments;
+
     response.send(stub);
 });
 
 //delete project
 router.delete('/:id', function (request, response) {
     var stub  = require('./project-stub.json');
+    var element;
+
+    for (var i = 0, len = stub.length; i < len; i++) {
+        if(stub[i].id == request.params.id) {
+            element = i;
+        }
+    }
+    stub.splice(element,1);
     response.send(stub);
 });
 
