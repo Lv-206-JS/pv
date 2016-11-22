635bddfe335980b21bc7dc29af00aeb085f3c7b0
Sikora Vitalii <vitalii.sikora@gmail.com>
2016-11-22 13:22:12 +0200

    required mongoose

--- rest/projects.js
+++ rest/projects.js
@@ -1,8 +1,8 @@
 'use strict';
-
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var db = connect("localhost:27017/ganttcharts");
 var Project  = require('../mongoose').ProjectModel;
 
 //functions for working with
