9b3f4d3a4fcad1df43ab3b0fd7efa86d0c15a88a
Anti-k <romanslyzebniy@gmail.com>
2016-11-23 12:15:01 +0200

    functional added

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ var flash = require('connect-flash');
 var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-
 var db = mongoose.connection;
 
 
@@ -25,6 +24,12 @@ var app = express();
 var projectAPI = require('./rest/routes/projects');
 var settingsAPI = require('./rest/routes/settings');
 var taskAPI = require('./rest/routes/tasks');
+
+
+
+//routes
+var routes = require('./rest/routes/index');
+var users = require('./rest/routes/users');
 
 // Enable CORS for unit tests
 app.use(function (request, response, next) {
@@ -39,6 +44,75 @@ app.use('/rest/tasks',  taskAPI);
 
 
 
+
+
+
+
+//View Engine
+app.set('views', path.join(__dirname, 'views'));//folder views handles our views
+app.engine('handlebars', exphbs({defaultLayout: 'layout'}));//setting engine and default layout
+app.set('view engine', 'handlebars');
+
+//BodyParser MiddleWare
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//Set Static folder(src folder);
+//stuff that is publicly accessible to the browser
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Express session
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+// Passport Initialization
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Express Validator code taken from express-validator github
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value){
+    var namespace = param.split('.')
+    , root = namespace.shift()
+    , formParam = root;
+
+    while(namespace.length){
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg : msg,
+      value : value
+    };
+  }
+}));
+
+//Connect Flash MiddleWare
+app.use(flash());
+
+//Global Variables For Flash Messages
+app.use(function(req, res, next){
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  //passpport own flash error messages
+  res.locals.error = req.flash('error');
+  res.locals.current_user = req.user || null
+  next();
+});
+
+
+
+
+
+
+app.use('/', routes);
+app.use('/users', users);
+
+
 app.use(/\/project.*/, express.static('./index.html'));
 app.use(express.static(__dirname));
 
@@ -48,6 +122,9 @@ app.use(function(req, res, next) {
     err.status = 404;
     next(err);
 });
+
+
+
 
 //exporting  app to fire www
 module.exports = app;
