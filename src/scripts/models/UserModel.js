bb8f64fb1d96061f2b43527a39dbfd9a62ff3721
skHazard <sk.hazard@gmail.com>
2016-11-26 15:19:50 +0200

    added

--- src/scripts/models/UserModel.js
+++ src/scripts/models/UserModel.js
@@ -4,17 +4,19 @@ define([
     'use strict';
 
     var UserModel = Backbone.Model.extend({
-        url: '/rest/user/1',
+        url: '/rest/user/691b9444-68a6-e4b6-b80c-731e6f18db52',
         setUrl: function (id) {
             this.url = '/rest/user/' + id;
         },
 
         defaults: {
+            'id': '0',
+            'name': 'test',
             'projectId': '0',
             'userId': '0',
             'role': 'author'
         }
     });
 
-    return UserModel;
+    return new UserModel();
 });
