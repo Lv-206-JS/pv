629f92f830b8c524c197d4276ac134564cd62aec
adstellas <_oljunya_@ukr.net>
2016-12-09 22:12:59 +0200

    Change task list render to render task row separately.

--- src/scripts/views/project/taskRow.js
+++ src/scripts/views/project/taskRow.js
@@ -1,1 +1,28 @@
-//TODO create TaskRowView
+define([
+        'backbone',
+        'underscore',
+        'JST'
+    ],
+    function (Backbone, _, JST) {
+        'use strict';
+
+        var TaskRowView = Backbone.View.extend({
+            template: JST['project:taskRow'],
+            className: 'table-row',
+
+            initialize: function (options) {
+                this.tasks = options.tasks;
+                this.task = options.task;
+            },
+
+            render: function () {
+                this.$el.html(this.template({
+                    tasks: this.tasks, task: this.task
+                }));
+                return this;
+            }
+
+        });
+
+        return TaskRowView;
+    });
--- src/scripts/views/project/tasksList.js
+++ src/scripts/views/project/tasksList.js
@@ -1,15 +1,15 @@
 define([
-        'backbone',
-        'underscore',
-        'JST'//,
-        //'views/project/taskRow'
-    ],
-    function (Backbone, _, JST) { //, TaskRowView) {
-        'use strict';
+                'backbone',
+                'underscore',
+                'JST',
+                'views/project/taskRow'
+            ],
+            function (Backbone, _, JST, TaskRowView) {
+                'use strict';
 
-        var TasksListView = Backbone.View.extend({
-            template: JST['project:tasksList'],
-            id: 'tasks-list',
+                var TasksListView = Backbone.View.extend({
+                    template: JST['project:tasksList'],
+                    id: 'tasks-list',
 
             events: {
                 'click .add-task': 'onTaskAdd',
@@ -20,14 +20,25 @@ define([
             initialize: function (options) {
                 this.model = options.model;
                 this.tasks = this.model.get('tasks');
-                //this.milestones = this.model.get('milestones');
             },
 
             render: function render() {
                 this.$el.html(this.template({
                     tasks: this.tasks
                 }));
+                this.renderTaskRows();
                 return this;
+            },
+
+            renderTaskRows: function () {
+                var task = null;
+                var lastElem = this.$el.find('.table-task-header');
+                for (var i = 0; i < this.tasks.length; i++) {
+                    task = this.tasks[i];
+                    this.taskRowView = new TaskRowView({tasks: this.tasks, task: task}).render();
+                    $(this.taskRowView.$el).insertAfter(lastElem);
+                    lastElem = this.$el.find('.table-row:last');
+                }
             },
 
             renderTaskView: function (id) {
