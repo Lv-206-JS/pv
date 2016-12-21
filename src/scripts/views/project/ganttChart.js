31ed9faa87bf8da2dbe3ec22fa67d002cdb0ad5c
adstellas <_oljunya_@ukr.net>
2016-12-21 15:40:01 +0200

    Working on Gantt Chart view.

--- src/scripts/views/project/ganttChart.js
+++ src/scripts/views/project/ganttChart.js
@@ -1,8 +1,10 @@
 define([
     'backbone',
     'JST',
+    'moment',
+    'timeLine',
     'views/project/ganttTaskRow'
-    ], function (Backbone, JST, GanttTaskRowView) {
+    ], function (Backbone, JST, Moment, TimeLineLib, GanttTaskRowView) {
         'use strict';
 
         var GanttChartView = Backbone.View.extend({
@@ -13,10 +15,13 @@ define([
                 this.model = options.model;
                 this.tasks = options.model.get('tasks');
                 this.milestones = options.model.get('milestones');
+                this.settings = options.model.get('settings');
+                this.dayStart = Number(this.settings.dayStart);
+                this.tasksPositions = (options.tasksPositions != undefined) ? options.tasksPositions : null;
             },
 
             render: function render() {
-                this.createGanttChartDateHeader;
+                this.createGanttChartDateHeader();
                 this.$el.html(this.template({
                     model: this.model, tasks: this.tasks, milestones: this.milestones
                 }));
@@ -25,9 +30,28 @@ define([
             },
 
             createGanttChartDateHeader: function () {
-                var topDates,
-                    bottomDates;
-                var projectHours;
+                var timeLine = new TimeLineLib(this.model);
+                // var topDates = [],
+                //     bottomDates = [],
+                //     zoom = 1,
+                //     hour = 50,
+                //     projectStartDate,
+                //     projectDuration;
+                // if (zoom == 1) { // if zoom == 100%
+                //
+                //     // bottomDates are hours
+                //     bottomDates[0]; //to Local Time
+                //     for (var i = 1; i < projectDuration; i++) {
+                //         bottomDates[i] = Moment(bottomDates[i-1]).add(1, 'h');
+                //     }
+                //     console.log(bottomDates);
+                //
+                //     //topDates are days
+                //     topDates[0] = projectStartDate;
+                //     for (i = 1; i < Moment.duration(projectDuration, 'h').asDays(); i++) {
+                //         topDates[i] = Moment(topDates[i-1]).add(1, 'd');
+                //     }
+                // }
             },
 
             renderTaskRows: function () {
@@ -35,7 +59,7 @@ define([
                 var lastElem = this.$el.find('.table-task-header');
                 for (var i = 0; i < this.tasks.length; i++) {
                     task = this.tasks[i];
-                    this.ganttTaskRowView = new GanttTaskRowView({tasks: this.tasks, task: task}).render();
+                    this.ganttTaskRowView = new GanttTaskRowView({task: task, tasksPositions: this.tasksPositions}).render();
                     $(this.ganttTaskRowView.$el).insertAfter(lastElem);
                     lastElem = this.$el.find('.table-task-row:last');
                 }
