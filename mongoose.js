6747bf78add6fff50e24bd09fa956fb98ce5826b
Katya Sharko <katyasharko@gmail.com>
2016-12-23 16:34:47 +0200

    fixed issue with task start date

--- mongoose.js
+++ mongoose.js
@@ -37,6 +37,7 @@ var ProjectsSchema = new Schema({
         projectId: {type: String, required: false},
         name: {type: String, required: false},
         description: {type: String, required: false},
+        startDate: {type: String, required: false},
         estimateTime: {type: Number, required: false},
         resource: {type: String, required: false},
         dependsOn: [{
