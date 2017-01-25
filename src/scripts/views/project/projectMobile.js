e23bdbba550d4447939fe8ee21225499e6c4ea47
skHazard <hazard.sk@gmail.com>
2017-01-26 00:46:24 +0200

    signout and backToList added in drawer

--- src/scripts/views/project/projectMobile.js
+++ src/scripts/views/project/projectMobile.js
@@ -16,11 +16,15 @@ define([
         template: JST['project:projectMobile'],
         className: 'mobile-project-view',
         events: {
-            'click .toggle-button': 'showDrawer'
+            'click .toggle-button': 'showDrawer',
+            'click .back-to-projects': 'onBackToProjects',
+            'click .sign-out-button': 'onSignOut'
         },
 
         initialize: function (projectId) {
-
+            this.userModel = PV.userModel;
+            this.userModel.setUrl('/rest/user');
+            this.userModel.fetch();
             this.projectId = projectId;
             this.model = new Model();
             this.model.setUrl(this.projectId);
@@ -31,7 +35,8 @@ define([
 
         render: function () {
             var project = this.model.toJSON();
-            this.$el.html(this.template({projectId: project}));
+            this.$el.html(this.template({projectId: project, name: this.model.get('name')}));
+            console.log(name);
             this.slideout = new Slideout({
                 'panel': document.getElementById('panel'),
                 'menu': document.getElementById('menu'),
@@ -49,6 +54,16 @@ define([
         showDrawer: function showDrawer() {
 
             this.slideout.toggle();
+        },
+
+        onBackToProjects: function onBackToProjects() {
+            PV.router.navigate('projects', {trigger: true});
+        },
+
+        onSignOut: function onSingOut() {
+            $.ajax({url: '/users/logout'});
+            PV.userModel.clear().set(this.userModel.defaults);
+            PV.router.navigate('/', {trigger: true});
         }
 
     });
